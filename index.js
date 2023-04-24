902a9f74cabeff76dc91053cb6854a744b2d109b
Sukanta Roy <78083659+itzFelu@users.noreply.github.com>
2023-04-24 06:34:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello')
+  res.send('<h1>This is my server<br><hr><hr></h1')
 })
 
 app.listen(4000, ()=>{
